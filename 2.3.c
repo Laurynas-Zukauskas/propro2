9640d2face8e83e8a03f68db8a4dbfda70772da9
Laurynas-Zukauskas <118725516+Laurynas-Zukauskas@users.noreply.github.com>
2022-11-26 15:58:30 +0200

    Add files via upload

---
+++ 2.3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    int a;
+    char b[100],c[]="lyginis",d[]="nelyginis";
+    scanf("%d",&a);
+    b[100] = (a%2==0) ? strcpy(b,c) : strcpy(b,d);
+    printf("%s",b);
+    return 0;
+}
