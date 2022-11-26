dcbd3c8b804e6574a6117515e73a69dd8c627508
Laurynas-Zukauskas <118725516+Laurynas-Zukauskas@users.noreply.github.com>
2022-11-26 15:57:49 +0200

    Add files via upload

---
+++ 2.1.c
@@ -0,0 +1,5 @@
+int main(){
+    char a=76,b=97,c=117,d=114,e=121,f=110,g=115,h=32,i=90,j=107;
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",a,b,c,d,e,f,b,g,h,i,c,j,b,c,g,j,b,g);
+    return 0;
+}
