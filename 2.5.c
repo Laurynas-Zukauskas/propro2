0ea7d076016c30d47fbff5c08395af23affdad97
Laurynas-Zukauskas <118725516+Laurynas-Zukauskas@users.noreply.github.com>
2022-11-26 15:58:44 +0200

    Add files via upload

---
+++ 2.5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double x,y,z,a,b;
+    scanf("%lf%lf%lf",&x,&y,&z);
+    a=x+4*y+pow(z,3);
+    b=(x+sqrt(y))*(pow(z,4)-fabs(z)+46.3);
+    printf("a) %f\nb) %f",a,b);
+    return 0;
+}
